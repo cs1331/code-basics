642acede842b34be8736e447f90e3ed46e4a6643
Chris Simpkins <chris.simpkins@gmail.com>
2016-01-15 10:02:57 -0500

    Add array code

--- CourseAverage.java
+++ CourseAverage.java
@@ -1,11 +1,34 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class CourseAverage {
 
     public static void main(String[] args) {
-        double homeworkAvg = 74.2;
-        double examAvg = (81 + 91 + 93) / 3;
-        double finalExam = 89;
-        double courseAverage = (.2 * homeworkAvg) + (.6 * examAvg)
-            + (.2 * finalExam);
-        System.out.println("Course Average: " + courseAverage);
+        int[] exams = new int[3];
+        double examAvg, hwAvg, finalExam, courseAvg;
+        Scanner gradeFile = null;
+        try {
+            gradeFile = new Scanner(new FileInputStream("grades.txt"));
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+            System.exit(0);
+        }
+        for (int i = 0; i < 3; ++i) {
+            exams[i] = gradeFile.nextInt();
+        }
+        examAvg = calcAverage(exams);
+        hwAvg = gradeFile.nextDouble();
+        finalExam = gradeFile.nextDouble();
+        courseAvg = .2 * hwAvg + .6 * examAvg + .2 * finalExam;
+        System.out.printf("Your course average is %.1f%n", courseAvg);
+    }
+
+    private static double calcAverage(int[] numbers) {
+        double sum = 0;
+        for (int number: numbers) {
+            sum += number;
+        }
+        return (double) sum / numbers.length;
     }
 }
